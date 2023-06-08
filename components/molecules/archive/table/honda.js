f276441652c4f5c99cb9d59a60423c24cb838d3b
Erwnyy <dbs10765@gmail.com>
2023-06-08 00:27:41 -0200

    final archive

--- components/molecules/archive/table/honda.js
+++ components/molecules/archive/table/honda.js
@@ -11,49 +11,6 @@ const Honda = () => {
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
             Front End 123
           </p>
-          <h1>Erwin Yuliyanto 123</h1>
-          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
-          </p>
-          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
-          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
-          </p>
-          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
-          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
-          </p>
-          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
-          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
-          </p>
-          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
-          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
-          </p>
-          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
-          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
-          </p>
-          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
-          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
-          </p>
-          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p>
         </div>
     </div>
   )
--- components/molecules/archive/table/table.js
+++ components/molecules/archive/table/table.js
@@ -15,31 +15,6 @@ const Table = () => {
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900 mb-4">
             Developer 123
           </p>
-          <h1>Erwin Yuliyanto 123</h1>
-          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
-          </p>
-          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
-          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
-          </p>
-          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
-          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
-          </p>
-          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
-          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
-          </p>
-          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p>
         </div>
       </div>
     </div>
