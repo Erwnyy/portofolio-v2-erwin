058441f6a28ca6828d732d5036c9913219e9c1a5
Erwnyy <dbs10765@gmail.com>
2023-06-08 00:26:52 -0200

    revisi dibagian arcvi

--- components/molecules/archive/table/honda.js
+++ components/molecules/archive/table/honda.js
@@ -4,55 +4,67 @@ const Honda = () => {
   return (
     <div>
       <div>
-          <h1>Erwin Yuliyanto 123</h1>
+          <h1>Erwin Yuliyanto 123123123</h1>
           <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
+            ERWIN YULIYANTO 123123123
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
+            Front End 123123123
           </p>
-          <h1>Erwin Yuliyanto 123</h1>
+          <h1>Erwin Yuliyanto </h1>
           <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
+            ERWIN YULIYANTO 
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
+            Front End 
+          </p>
+          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900 mb-4">
+            Developer 
+          </p><h1>Erwin Yuliyanto </h1>
           <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
+            ERWIN YULIYANTO 
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
+            Front End 
+          </p>
+          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900 mb-4">
+            Developer 
+          </p><h1>Erwin Yuliyanto </h1>
           <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
+            ERWIN YULIYANTO 
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
+            Front End 
+          </p>
+          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900 mb-4">
+            Developer 
+          </p><h1>Erwin Yuliyanto </h1>
           <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
+            ERWIN YULIYANTO 
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
+            Front End 
+          </p>
+          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900 mb-4">
+            Developer 
+          </p><h1>Erwin Yuliyanto </h1>
           <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
+            ERWIN YULIYANTO 
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
+            Front End 
+          </p>
+          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900 mb-4">
+            Developer 
+          </p><h1>Erwin Yuliyanto </h1>
           <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
+            ERWIN YULIYANTO 
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
-          </p><h1>Erwin Yuliyanto 123</h1>
-          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
+            Front End 
           </p>
-          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
+          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900 mb-4">
+            Developer 
           </p>
         </div>
     </div>
