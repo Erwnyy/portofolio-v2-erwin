f84c5783d7fdbada21f8f15393495da0047a7d8c
Erwnyy <dbs10765@gmail.com>
2023-06-08 00:28:25 -0200

    final table

--- components/molecules/archive/table/honda.js
+++ components/molecules/archive/table/honda.js
@@ -4,12 +4,12 @@ const Honda = () => {
   return (
     <div>
       <div>
-          <h1>Erwin Yuliyanto 123</h1>
+          <h1>Erwin Yuliyanto </h1>
           <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123
+            ERWIN YULIYANTO 
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123
+            Front End 
           </p>
         </div>
     </div>
