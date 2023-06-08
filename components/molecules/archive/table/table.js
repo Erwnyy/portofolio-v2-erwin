7e72e55ad7037a35e7ae2a676079d19e118b0228
Erwnyy <dbs10765@gmail.com>
2023-06-08 00:17:35 -0200

    final

--- components/molecules/archive/table/table.js
+++ components/molecules/archive/table/table.js
@@ -5,15 +5,15 @@ const Table = () => {
     <div>
       <div>
         <div>
-          <h1>Erwin Yuliyanto 123123</h1>
+          <h1>Erwin Yuliyanto</h1>
           <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO 123123
+            ERWIN YULIYANTO
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End 123123
+            Front End
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900 mb-4">
-            Developer 123123
+            Developer
           </p>
         </div>
       </div>
