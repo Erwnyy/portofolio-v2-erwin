b4bf55747a0aa59a35f79d3ca4dbc2988a242e1f
Erwnyy <dbs10765@gmail.com>
2023-06-08 00:05:25 -0200

    final

--- components/molecules/archive/table/honda.js
+++ components/molecules/archive/table/honda.js
@@ -3,10 +3,18 @@ import React from 'react'
 const Honda = () => {
   return (
     <div>
-      cek123123123123123123
       <div>
-            erwin uppercase
-    </div>
+          <h1>Erwin Yuliyanto</h1>
+          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
+            ERWIN YULIYANTO 123123
+          </p>
+          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
+            Front End 123123
+          </p>
+          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900 mb-4">
+            Developer 123123
+          </p>
+        </div>
     </div>
   )
 }
--- components/molecules/archive/table/table.js
+++ components/molecules/archive/table/table.js
@@ -5,17 +5,16 @@ const Table = () => {
     <div>
       <div>
         <div>
-          <h1>Erwin Yuliyanto12312123</h1>
+          <h1>Erwin Yuliyanto</h1>
           <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTOawdawdawdawd
+            ERWIN YULIYANTO 123123
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front Endawdawdawdawd
+            Front End 123123
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900 mb-4">
-            Developerawdawdawdawdawdawd
+            Developer 123123
           </p>
-          123123123 cek123
         </div>
       </div>
     </div>
