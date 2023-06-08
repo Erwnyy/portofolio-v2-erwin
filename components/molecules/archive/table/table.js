42722cd6c3cbcdc3c82d678cb641ea67e21a32d9
Erwnyy <dbs10765@gmail.com>
2023-06-08 00:21:59 -0200

    erwin 123

--- components/molecules/archive/table/table.js
+++ components/molecules/archive/table/table.js
@@ -5,15 +5,40 @@ const Table = () => {
     <div>
       <div>
         <div>
-          <h1>Erwin Yuliyanto</h1>
+          <h1>Erwin Yuliyanto 123</h1>
           <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
-            ERWIN YULIYANTO
+            ERWIN YULIYANTO 123
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
-            Front End
+            Front End 123
           </p>
           <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900 mb-4">
-            Developer
+            Developer 123
+          </p>
+          <h1>Erwin Yuliyanto 123</h1>
+          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
+            ERWIN YULIYANTO 123
+          </p>
+          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
+            Front End 123
+          </p><h1>Erwin Yuliyanto 123</h1>
+          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
+            ERWIN YULIYANTO 123
+          </p>
+          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
+            Front End 123
+          </p><h1>Erwin Yuliyanto 123</h1>
+          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
+            ERWIN YULIYANTO 123
+          </p>
+          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
+            Front End 123
+          </p><h1>Erwin Yuliyanto 123</h1>
+          <p className="uppercase font-normal text-xl md:text-lg tracking-[9px] text-neutral-400 ml-1">
+            ERWIN YULIYANTO 123
+          </p>
+          <p className="font-bold text-[5rem] md:text-[3rem] text-neutral-900">
+            Front End 123
           </p>
         </div>
       </div>
