c5e51e459ac8fccf47812a90a08920bf827c124e
Erwnyy <dbs10765@gmail.com>
2023-06-07 23:58:53 -0200

    revisi dibagian developer

---
+++ components/molecules/archive/table/honda.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Honda = () => {
+  return (
+    <div>
+      cek123123123123123123
+      <div>
+            erwin uppercase
+    </div>
+    </div>
+  )
+}
+
+export default Honda
